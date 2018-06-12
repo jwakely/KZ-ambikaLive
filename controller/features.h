4fca75ef7309bd82d4a2cb765e0a053a8d8a221e
Carey O'Sullivan <kez@tijuanacartel.com>
2018-06-12 20:14:00 +1000

    Switches to disable features

---
+++ controller/features.h
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------------
+//
+// KEZ MOD: Switches to disable features and recover some bytes.
+//
+// -----------------------------------------------------------------------------
+
+#define DIRTY_CC_LOOKUP
+
+#define DISABLE_CARD_INFO_PAGE
+//#define DISABLE_VERSION_MANAGER
+//#define DISABLE_RAGAS
+//#define DISABLE_SEQUENCER
+//#define DISABLE_ARPEGGIO
+//#define DISABLE_LAUNCHKEY_MODE
