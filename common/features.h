92cf1bfbe29288e1bbd1fff85de32b42ab7d6295
Carey O'Sullivan <kez@tijuanacartel.com>
2020-01-16 11:37:46 +1000

    Move features.h to common/ instead of controller/

--- common/features.h
+++ common/features.h
@@ -4,11 +4,15 @@
 //
 // -----------------------------------------------------------------------------
 
-#define DIRTY_CC_LOOKUP
-
 #define DISABLE_CARD_INFO_PAGE
 //#define DISABLE_VERSION_MANAGER
 //#define DISABLE_RAGAS
+//#define DISABLE_GROOVE_TEMPLATES
 //#define DISABLE_SEQUENCER
 //#define DISABLE_ARPEGGIO
-//#define DISABLE_LAUNCHKEY_MODE
+//#define LFO_MEMORY_DIET
+//#define BANDLIMITED_TRIANGLE
+#define DIRTY_CC_LOOKUP
+#define DISABLE_CC_MAPS
+#define DISABLE_LAUNCHKEY_MODE
+#define DISABLE_PART_MUTES
